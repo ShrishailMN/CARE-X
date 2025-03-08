73e062d89da168abcee73b37e498a711f564b359
Shrishail <141026479+ShrishailMN@users.noreply.github.com>
2025-03-08 16:00:44 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,9 @@
 # gunicorn_config.py
-workers = 2
+bind = "0.0.0.0:10000"
+workers = 1
 threads = 2
-timeout = 120
+timeout = 300
 worker_class = 'gthread'
 max_requests = 1000
 max_requests_jitter = 50
+preload_app = True
